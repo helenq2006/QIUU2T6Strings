c6cc0bc305683820e3d7bf594d502e5ff71b25d6
unknown <BT_3W2_31@BKTECH.LOCAL>
2022-10-11 09:27:55 -0400

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,21 @@
+public class Cat {
+    public static void main(String[] args)
+    {
+        System.out.println("My cat says \"meow\" when she is happy!");
+        System.out.println("Cows say \"moo\"");
+        System.out.println("Brooklyn Tech \n29 Ft. Greene\nBrooklyn");
+        System.out.println("An \"i\"\nfor\nan eye");
+        System.out.println("The escape sequence for \" is \"\"");
+        System.out.println("The escape sequence for \" is \\");
+        System.out.println("/A\\V/A\\\n\\V/A\\V/");
+        System.out.println("Use \"\\n\"\nfor a new line.");
+        System.out.println("My favorite punctuation mark is \"\\\"");
+        System.out.println("Now I know my \nA \n" + "  " + "B \n" + "    " + "C's");
+        System.out.println("¯\\_(\"/)_/¯");
+        System.out.println("Dear Jen,\nYou got an \"A\"!\n\nSincerely,\nThe teacher");
+        System.out.println(" * *\n* * *\n * *");
+        System.out.println("Hi \"\\\"\n yay");
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String str1 = "Minions";
+        String str2 = new String("Bob");
+        String s = "25";
+        String str3 = new String(s);
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+
+
+    }
+}
+
